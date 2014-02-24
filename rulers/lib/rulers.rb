49f4d2a1c022b3e0086ca28126cd0df7cc5caefb
Ramin <rsedighi@gmail.com>
2014-02-23 17:56:12 -0800

    Excercise 2

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -1,11 +1,26 @@
-require "rulers/array"
+require 'rulers/version'
+require 'rulers/routing'
+
 
 module Rulers
   class Application
     def call(env)
-      `echo debug > debug.txt`;
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404,
+          {'Content-Type' => 'text/html'},[]]
+      end
+      if env['PATH_INFO'] == '/'
+        return [301, {'Content-Type' => 'text/html', 'Location' => '/quotes/a_quote'},[]]
+      end
+      begin
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [200, {'Content-Type' => 'text/html'},
-        ["Hello from Ruby on Rulers!"]]
+        [text]]
+      rescue Exception
+        [500, {'Content-Type' => 'text/html'}, ['ERROR']]
+      end
     end
   end
 end
