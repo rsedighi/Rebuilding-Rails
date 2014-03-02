09420710ca3fd12f77b4a9fa77a070fbc4cc4585
Ramin <rsedighi@gmail.com>
2014-03-01 18:49:43 -0800

    Chapter 6

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -15,15 +15,15 @@ module Rulers
       if env['PATH_INFO'] == '/'
         return [301, {'Content-Type' => 'text/html', 'Location' => '/quotes/a_quote'},[]]
       end
-      begin
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'},
-        [text]]
-      rescue Exception => e
-        error = e.inspect + "\n" + e.backtrace.join("\n")
-        [500, {'Content-Type' => 'text/text'}, [error]]
+      if controller.get_response
+        st, hd, rs = controller.get_response.to_a
+        [st, hd, [rs.body].flatten]
+      else[200,
+            {'Content-Type' => 'text/html'}, [text]]
       end
     end
   end
--- rulers/lib/rulers/controller.rb
+++ rulers/lib/rulers/controller.rb
@@ -1,5 +1,6 @@
 require "erubis"
 require "rulers/file_model"
+require "rack/request"
 
 module Rulers
   class Controller
@@ -7,6 +8,28 @@ module Rulers
 
     def initialize(env)
       @env = env
+    end
+
+    def request
+      @request ||= Rack::Request.new(@env)
+    end
+
+    def response(text, status = 200, headers = {})
+      raise "Already responded!" if @response
+      a = [text].flatten
+      @response = Rack::Response.new(a, status, headers)
+    end
+
+    def get_response
+      @response
+    end
+
+    def render_response(*args)
+      response(render(*args))
+    end
+
+    def params
+      request.params
     end
 
     def env
@@ -18,7 +41,10 @@ module Rulers
         controller_name, "#{view_name}.html.erb"
       template = File.read filename
       eruby = Erubis::Eruby.new(template)
-      eruby.result locals.merge(:env => env)
+      instance_variables.each do |i|
+        eruby.instance_variable_set(i, instance_variable_get(i))
+      end
+      # eruby.result locals.merge(:env => env)
     end
 
     def controller_name
